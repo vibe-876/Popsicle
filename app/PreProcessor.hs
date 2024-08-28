986dfbc0a71e2cdfb86fb19fbc401b125dd7f4c6
cam <ayresc17@holyportcollege.org.uk>
2024-08-28 10:35:25 +0100

    Changed the preprocessor into a literate org file.

--- app/PreProcessor.hs
+++ app/PreProcessor.hs
@@ -1,15 +1,8 @@
-{-
-All that this is going to do for now is remove
-comments from the program.
-
-I might add more later...
--}
 module PreProcessor where
 
 
 preProcess :: String -> String
 preProcess = removeComs
-
 
 removeComs :: String -> String
 removeComs []       = []
