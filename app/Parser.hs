23a4bef3aefd62152b2504518929b94b71b9d367
cam <ayresc17@holyportcollege.org.uk>
2024-08-28 11:10:05 +0100

    Changed the parser into an org file.

--- app/Parser.hs
+++ app/Parser.hs
@@ -1,7 +1,4 @@
-{-# LANGUAGE FlexibleContexts #-}
 module Parser where
 
 import qualified Text.Parsec as Parsec
 import Text.Parsec ((<?>))
-import Control.Applicative
-import Control.Monad.Identity (Identity)
